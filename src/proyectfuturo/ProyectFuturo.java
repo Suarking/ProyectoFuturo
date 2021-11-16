6ec17c283386969baeba241dc8078488acb4a7b4
Suarking <fn7sephiroth@gmail.com>
2021-11-16 20:49:53 +0100

    Public

---
+++ src/proyectfuturo/ProyectFuturo.java
@@ -0,0 +1,28 @@
+
+package proyectfuturo;
+
+public class ProyectFuturo {
+
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Estudiante : " + args[0]);
+        System.out.println(veredicto(args[1], args[2]));
+        
+    }
+    static String veredicto(String a, String b) {
+        
+        
+        if (a.equals(b)) {
+    return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+}
+        else if (Integer.valueOf(a) > Integer.valueOf(b)){
+    return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+}
+        else{
+    return "Ideal. Trabajas los contenidos en casa.";        
+        }
+    }
+}
+    
